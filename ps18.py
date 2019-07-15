32e0c5d0f788683ca08f43c570978f31e721212c
ragavanrocky <52520783+ragavanrocky@users.noreply.github.com>
2019-07-15 15:46:59 +0530

    Create ps18.py

---
+++ ps18.py
@@ -0,0 +1,12 @@
+
+import math
+a,b=map(int,input().split())
+li=list(map(int,input().split()))
+p=[]
+for i in range(0,b):
+    l,r=map(int,input().split())
+    p.append([l,r])
+for j in p:
+    e=j[0]-1
+    f=j[1]-1
+    print(math.gcd(li[e],li[f]))
